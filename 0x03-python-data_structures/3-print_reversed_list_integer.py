9ed3470630970a12f4b9a2c22890d9feaa81187e
sixtusagbo <miracleagbosixtus@gmail.com>
2022-09-26 23:50:42 +0100

    [Fix] Not allowed to cast integer into string

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    for num in my_list[::-1]:
-        print("{}".format(num))
+    if not my_list:
+        pass
+    else:
+        for num in my_list[::-1]:
+            print("{:d}".format(num))
