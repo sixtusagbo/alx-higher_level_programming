7deccded8cdc27b40ce74b184b3a8ee750716bc5
sixtusagbo <miracleagbosixtus@gmail.com>
2022-09-27 00:48:05 +0100

    [Feature] Remove all c and C from a string

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    j = 0
+    new_string = my_string[:]
+
+    for i in range(len(my_string)):
+        if (my_string[i] in "cC"):
+            new_string = new_string[:(i - j)] + my_string[(i + 1):]
+            j += 1
+
+    return (new_string)
