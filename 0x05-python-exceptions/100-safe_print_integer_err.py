756377867782ab2a4370f565848b4df225e2c5be
sixtusagbo <miracleagbosixtus@gmail.com>
2022-10-12 01:26:30 +0100

    [Feature] Print error message to stderr

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_integer_err(value):
+    from sys import stderr
+
+    try:
+        print("{:d}".format(value))
+    except Exception as exc:
+        print('Exception: {}'.format(exc), file=stderr)
+        return False
+
+    return True
