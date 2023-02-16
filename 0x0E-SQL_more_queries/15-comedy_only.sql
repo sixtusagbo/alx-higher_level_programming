a74a07080fa61ea4a353144bf7cd8e1452c2c26b
sixtusagbo <miracleagbosixtus@gmail.com>
2023-02-16 23:38:15 +0100

    [Feature] Only Comedy

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,9 @@
+-- Lists all Comedy shows in the database hbtn_0d_tvshows
+SELECT ts.title
+FROM tv_shows AS ts
+	JOIN tv_show_genres AS tsg
+		ON ts.id = tsg.show_id
+	JOIN tv_genres AS tg
+		ON tg.id = tsg.genre_id
+WHERE tg.name = "Comedy"
+ORDER BY ts.title;
