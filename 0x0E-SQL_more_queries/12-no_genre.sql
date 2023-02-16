f6c0d96de5cbc829df73097f9bff72759301c347
sixtusagbo <miracleagbosixtus@gmail.com>
2023-02-16 10:48:39 +0100

    [Update] = to is

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -3,5 +3,5 @@ SELECT ts.title, tsg.genre_id
 FROM tv_shows AS ts
 LEFT JOIN tv_show_genres AS tsg
 		ON ts.id = tsg.show_id
-WHERE tsg.genre_id = NULL
+WHERE tsg.genre_id is NULL
 ORDER BY ts.title, tsg.genre_id;
