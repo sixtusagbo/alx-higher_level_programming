56bf378a1ff7307cd43408c9ec75b59cb66e11cc
sixtusagbo <miracleagbosixtus@gmail.com>
2023-02-16 10:46:07 +0100

    [Feature] No genre

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,7 @@
+-- Lists all shows contained in hbtn_0d_tvshows without a genre linked
+SELECT ts.title, tsg.genre_id
+FROM tv_shows AS ts
+LEFT JOIN tv_show_genres AS tsg
+		ON ts.id = tsg.show_id
+WHERE tsg.genre_id = NULL
+ORDER BY ts.title, tsg.genre_id;
