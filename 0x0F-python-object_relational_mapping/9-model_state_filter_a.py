ef3fadf849c68ee79f02c988b49f9566c8d33115
sixtusagbo <miracleagbosixtus@gmail.com>
2023-02-20 23:41:12 +0100

    [Feature] Contains `a`

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -3,17 +3,18 @@
 Prints the first State object from the database hbtn_0e_6_usa
 """
 from sys import argv
-from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
                             argv[1], argv[2], argv[3], pool_pre_ping=True))
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state = session.query(State).order_by(State.id).first()
+    states = session.query(State).filter(State.name.like("%a%")).all()
 
-    print("Nothing" if not state else "{}: {}".format(state.id, state.name))
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
