531adaa4d8486032f3a6d3dc7c1a50777c466505
sixtusagbo <miracleagbosixtus@gmail.com>
2023-02-21 00:17:51 +0100

    [Update] variable name

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Changes the name of a State object from the database hbtn_0e_6_usa
+Deletes all State objects with a name containing the letter a
+from the database hbtn_0e_6_usa
 """
 from sys import argv
 from sqlalchemy import create_engine
@@ -14,7 +15,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state = session.query(State).filter(State.name.like("%a%")).all()
+    states = session.query(State).filter(State.name.like("%a%")).all()
 
     for state in states:
         session.delete(state)
