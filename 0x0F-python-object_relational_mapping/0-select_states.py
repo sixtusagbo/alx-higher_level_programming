0b6d4795cab8ebcb7d608aec4e0492ebadd88cc2
sixtusagbo <miracleagbosixtus@gmail.com>
2023-02-20 01:58:20 +0100

    [Update] Pycodestyle

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,7 +8,7 @@ from sys import argv
 
 if __name__ == '__main__':
     # Connect to db
-    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3], port=3306);
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3], port=3306)
 
     # Grab the cursor
     cur = db.cursor()
