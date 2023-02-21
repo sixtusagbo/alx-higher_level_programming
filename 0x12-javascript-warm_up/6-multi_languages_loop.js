3b1da4482064859d2d3312e44ae5f8a778f890ff
sixtusagbo <miracleagbosixtus@gmail.com>
2023-02-21 05:30:16 +0100

    [Update] Variable name

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-const myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const messages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (let i = 0; i < myArray.length; i++) {
-  console.log(myArray[i]);
+for (let i = 0; i < messages.length; i++) {
+  console.log(messages[i]);
 }
