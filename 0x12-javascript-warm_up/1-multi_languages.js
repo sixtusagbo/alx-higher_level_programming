d039a21bbfdbff3996a157592a39f5eb27c157ce
sixtusagbo <miracleagbosixtus@gmail.com>
2023-02-21 03:57:50 +0100

    [Feature] 3 languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const expression1 = 'C is fun';
+const expression2 = 'Python is cool';
+const expression3 = 'JavaScript is amazing';
+
+console.log(expression1);
+console.log(expression2);
+console.log(expression3);
