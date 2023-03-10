8cfd7662a975b96de295d70b99b583d7aa5fd471
sixtusagbo <miracleagbosixtus@gmail.com>
2023-03-10 02:26:03 +0100

    [Feature] cURL to the end

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a request to a URL, and displays the size of the body of the response
-curl -sI $1 | grep 'Content-Length:' | cut -d " " -f 2
+curl -sI "$1" | grep 'Content-Length:' | cut -d " " -f 2
---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Sends a request to a URL and display only body of a 200 status code response
+curl -sL "$1"
