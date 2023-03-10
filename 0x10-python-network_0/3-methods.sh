626624838f68cfd098b20acc0f9081f2a92b4a24
sixtusagbo <miracleagbosixtus@gmail.com>
2023-03-10 11:16:02 +0100

    [Update] Get only methods to the end

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Displays all the HTTP mettods the server will accept
-curl -sIX OPTIONS "$1" | grep "Allow: " | cut -d " " -f 2
+curl -sIX OPTIONS "$1" | grep "Allow: " | cut -d " " -f 2-
