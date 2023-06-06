5e74bfedde097e86cfd3f63905f6ea6de96523ee
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-06-06 21:30:48 +0000

    Fetche and list the title for all star wars movies

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,8 @@
+$.getJSON(
+  'https://swapi-api.alx-tools.com/api/films/?format=json',
+  function (data, textStatus, jqXHR) {
+    $.each(data.results, function (index, value) {
+      $('UL#list_movies').append(`<li>${value.title}</li>`);
+    });
+  }
+);
